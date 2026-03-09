44567e5c58a75e13afe1c11be73e025d69befb9e
Batfino <151618376+Batfino@users.noreply.github.com>
2026-03-09 13:31:18 +0100

    movement

---
+++ My project/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+    public float smoothSpeed = 5f;
+
+    void LateUpdate()
+    {
+        Vector3 targetPosition = player.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetPosition, smoothSpeed * Time.deltaTime);
+    }
+}
--- My project/Assets/Scripts/CubeMovement.cs
+++ My project/Assets/Scripts/CubeMovement.cs
@@ -2,15 +2,52 @@ using UnityEngine;
 
 public class CubeMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float forwardSpeed = 5f;
+    public float sideSpeed = 5f;
+    public float jumpForce = 5f;
+
+    private Rigidbody rb;
+    private bool isGrounded = true;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Constant forward movement
+        float moveX = forwardSpeed * Time.deltaTime;
+
+        // Side movement
+        float moveZ = 0f;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveZ = sideSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveZ = -sideSpeed * Time.deltaTime;
+        }
+
+        transform.Translate(moveX, 0f, moveZ, Space.World);
+
+        // Jump when pressing space
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // Detect ground
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
     }
 }
