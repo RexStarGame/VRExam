193082df2c45a00b10c14b8ff99d762326e26954
Lolkinglolof <Torben.Thiemer@gmail.com>
2026-03-09 14:03:27 +0100

    Update Menu.cs

--- My project/Assets/Script/UI-Menu/Menu.cs
+++ My project/Assets/Script/UI-Menu/Menu.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
     private static Menu instance;
+    [SerializeField] private float timeToSwitchScenes = 5;
     private void Start()
     {
         if (instance == null)
@@ -16,6 +18,11 @@ public class Menu : MonoBehaviour
     }
     public void OnDeath()
     {
-
+        // a death animation maybe?
+        Invoke(nameof(MainMenu), timeToSwitchScenes);
+    }
+    private void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 }
