b88ee29046f8aa7f1957dfddc1b8d4c6febcc4cd
Patrick <63421519+RexStarGame@users.noreply.github.com>
2026-03-11 10:07:06 +0100

    fixed JumpUPS

--- My project/Assets/Script/Player/OneTimeJumpBoost.cs
+++ My project/Assets/Script/Player/OneTimeJumpBoost.cs
@@ -6,24 +6,37 @@ public class OneTimeJumpBoost : MonoBehaviour
     [Tooltip("How much force to apply when the player jumps.")]
     public float superJumpForce = 15f;
 
-    // OnTriggerStay runs every frame the player is inside the collider
-    private void OnTriggerStay(Collider other)
+    private bool isPlayerInZone = false;
+    private Rigidbody playerRb;
+
+    private void Update()
+    {
+        if (isPlayerInZone && Input.GetKeyDown(KeyCode.Space))
+        {
+            if (playerRb != null)
+            {
+                playerRb.linearVelocity = new Vector3(playerRb.linearVelocity.x, 0, playerRb.linearVelocity.z);
+
+                playerRb.AddForce(Vector3.up * superJumpForce, ForceMode.Impulse);
+
+                Destroy(gameObject);
+            }
+        }
+    }
+    private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                Rigidbody playerRb = other.GetComponent<Rigidbody>();
-
-                if (playerRb != null)
-                {
-                    playerRb.linearVelocity = new Vector3(playerRb.linearVelocity.x, 0, playerRb.linearVelocity.z);
-
-                    playerRb.AddForce(Vector3.up * superJumpForce, ForceMode.Impulse);
-
-                    Destroy(gameObject);
-                }
-            }
+            isPlayerInZone = true;
+            playerRb = other.GetComponent<Rigidbody>();
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerInZone = false;
+            playerRb = null;
         }
     }
 }
