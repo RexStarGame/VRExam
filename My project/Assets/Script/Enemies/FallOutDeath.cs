af57cbf2f2ad134030ad3583133520709c2a5b2f
Patrick <63421519+RexStarGame@users.noreply.github.com>
2026-03-16 11:15:23 +0100

    player dies if fall out of the map

---
+++ My project/Assets/Script/Enemies/FallOutDeath.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class FallOutDeath : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.collider.name); 
+
+        if (!collision.gameObject.CompareTag("Player"))
+            return;
+
+        HitDirection hitDir = ReturnDirection(collision);
+        Debug.Log(collision.collider.name + " hit" + hitDir);
+
+        if (hitDir != HitDirection.Top && hitDir != HitDirection.None) // hvis Hitdir er bottom eller none så er spilleren ikke død.
+        {
+            CubeMovement cubeMovement = collision.collider.GetComponentInParent<CubeMovement>();
+            if (cubeMovement != null)
+                cubeMovement.enabled = false;
+
+            Rigidbody playerRb = collision.collider.GetComponentInParent<Rigidbody>();
+            if (playerRb != null)
+            {
+                playerRb.linearVelocity = Vector3.zero;
+                playerRb.angularVelocity = Vector3.zero;
+            }
+
+            PlayerEvents.instance.DeathEvent.Invoke();
+        }
+    }
+
+    private enum HitDirection { None, Top, Bottom, Forward, Back, Left, Right } 
+
+    private HitDirection ReturnDirection(Collision collision)
+    {
+
+        HitDirection hitDirection = HitDirection.None;
+
+        Vector3 normal = collision.GetContact(0).normal;
+
+        if (Vector3.Angle(normal, transform.up) < 45f) { hitDirection = HitDirection.Top; }
+        else if (Vector3.Angle(normal, -transform.up) < 45f) { hitDirection = HitDirection.Bottom; }
+        else if (Vector3.Angle(normal, transform.forward) < 45f) { hitDirection = HitDirection.Forward; }
+        else if (Vector3.Angle(normal, -transform.forward) < 45f) { hitDirection = HitDirection.Back; }
+        else if (Vector3.Angle(normal, transform.right) < 45f) { hitDirection = HitDirection.Right; }
+        else if (Vector3.Angle(normal, -transform.right) < 45f) { hitDirection = HitDirection.Left; }
+
+        return hitDirection;
+    }
+}
