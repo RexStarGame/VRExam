5be99cf61a4ff8f2934833c76a7231f46f3ebad7
Lolkinglolof <Torben.Thiemer@gmail.com>
2026-03-16 11:53:06 +0100

    Update PlayerAnimation.cs

--- My project/Assets/Script/Player/PlayerAnimation.cs
+++ My project/Assets/Script/Player/PlayerAnimation.cs
@@ -2,15 +2,41 @@ using UnityEngine;
 
 public class PlayerAnimation : MonoBehaviour
 {
+    [SerializeField] public int CubesPerAxis = 8;
+    [SerializeField] public float Delay = 1f, Force = 300f, Radius = 2f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Invoke(nameof(Main), Delay);
     }
-
-    // Update is called once per frame
-    void Update()
+    void Main()
     {
-        
+        for (int x = 0; x < CubesPerAxis; x++)
+        {
+            for (int y = 0; y < CubesPerAxis; y++)
+            {
+                for (int z = 0; z < CubesPerAxis; z++)
+                {
+                    Vector3 coordinates = new Vector3(x, y, z);
+                    CreateCube(coordinates);
+                }
+            }
+        }
+        gameObject.SetActive(false);
+    }
+    void CreateCube(Vector3 coordinates)
+    {
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+        Renderer rd = cube.GetComponent<Renderer>();
+        rd.material = cube.GetComponent<Material>();
+
+        cube.transform.localScale = transform.localScale / CubesPerAxis;
+
+        Vector3 firstCubePos = transform.position - transform.localScale / 2 + cube.transform.localScale / 2;
+        cube.transform.position = firstCubePos + Vector3.Scale(coordinates,cube.transform.localScale);
+
+        Rigidbody rb = cube.AddComponent<Rigidbody>();
+        rb.AddExplosionForce(Force, transform.position, Radius);
     }
 }
